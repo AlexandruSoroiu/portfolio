840ea8d0cb76373545ec0ad213a54467f006da01
AlexandruSoroiu <alexandru.soroiu@wagner.edu>
2024-07-23 14:07:57 +0300

    designed the homepage
    updated the html, css, and js file so that the homepage would look nice

---
+++ assets/js/main.js
@@ -0,0 +1,27 @@
+function myMenuFunction(){
+    var menuBtn = document.getElementById("myNavMenu");
+
+    if(menuBtn.className === "nav-menu"){
+        menuBtn.btn.className += " responsive";
+    } else {
+        menuBtn.className = "nav-menu";
+    }
+}
+
+window.onscroll = function() {headerShadow()};
+
+function headerShadow() {
+    const navHeader = document.getElementById("header");
+
+    if (document.body.scrollTop > 50 || document.documentElement.scrollTop > 50) {
+        navHeader.style.boxShadow = "0 1px 6px rgba(0, 0, 0, 0.1)";
+        navHeader.style.height = "70px";
+        navHeader.style.lineHeight = "70px";
+    } else {
+
+        navHeader.style.boxShadow = "none";
+        navHeader.style.height = "90px";
+        navHeader.style.lineHeight = "90px";
+
+    }
+}
