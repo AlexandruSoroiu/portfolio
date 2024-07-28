d7f06641acc2026ddde3e87267580646981a0b1c
AlexandruSoroiu <alexandru.soroiu@wagner.edu>
2024-07-28 22:48:26 +0300

    finalized the website

--- assets/js/main.js
+++ assets/js/main.js
@@ -2,7 +2,7 @@ function myMenuFunction(){
     var menuBtn = document.getElementById("myNavMenu");
 
     if(menuBtn.className === "nav-menu"){
-        menuBtn.btn.className += " responsive";
+        menuBtn.className += " responsive";
     } else {
         menuBtn.className = "nav-menu";
     }
@@ -25,3 +25,87 @@ function headerShadow() {
 
     }
 }
+
+var typingEffect = new Typed(".typedText",{
+    strings : ["Student", "Survivor", "Problem-Solver"],
+    loop: true,
+    typeSpeed : 100,
+    backSpeed : 80,
+    backDelay : 2000
+})
+
+const sr = ScrollReveal({
+    origin: 'top',
+    distance: '80px',
+    duration: 2000,
+    reset: true
+})
+
+
+sr.reveal('.featured-text-card',{})
+sr.reveal('.featured-name',{delay: 100})
+sr.reveal('.featured-text-info',{delay: 200})
+sr.reveal('.featured-text-btn',{delay: 200})
+sr.reveal('.social-icons',{delay: 200})
+sr.reveal('.featured-image',{delay: 300})
+
+
+sr.reveal('.project-box',{interval: 200})
+
+sr.reveal('.top-header',{})
+
+const srLeft = ScrollReveal({
+    origin: 'left',
+    distance: '80px',
+    duration: 2000,
+    reset: true
+})
+
+srLeft.reveal('.about-info',{delay: 100})
+srLeft.reveal('.contact-info',{delay: 100})
+
+const srRight = ScrollReveal({
+    origin: 'right',
+    distance: '80px',
+    duration: 2000,
+    reset: true
+})
+
+srRight.reveal('.skills-box',{delay: 100})
+srRight.reveal('.form-control',{delay: 100})
+
+const sections = document.querySelectorAll('section[id]')
+
+function scrollActive() {
+    const scrollY = window.scrollY;
+    sections.forEach(current =>{
+        const sectionHeight = current.offsetHeight,
+            sectionTop = current.offsetTop - 50,
+          sectionId = current.getAttribute('id');
+
+        if(scrollY > sectionTop && scrollY <= sectionTop + sectionHeight) {
+            document.querySelector('.nav-menu a[href*=' + sectionId + ']').classList.add('active-link')
+        } else {
+            document.querySelector('.nav-menu a[href*=' + sectionId + ']').classList.remove('active-link')
+        }
+    })
+}
+
+function openInstagram() {
+    window.open("https://www.instagram.com/alex.soroiu/");
+}
+
+function openLinkedin() {
+    window.open("https://www.linkedin.com/in/alexandrudanielsoroiu/");
+}
+
+function openGithub() {
+    window.open("https://github.com/AlexandruSoroiu");
+}
+
+function openResume() {
+    window.open("/Alexandru_Soroiu_Resume.pdf")
+}
+
+
+window.addEventListener("scroll", scrollActive())
