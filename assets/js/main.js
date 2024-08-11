b11fc86fe7ab90ee20382fc5c7cc3ea48799c4c8
AlexandruSoroiu <alexandru.soroiu@wagner.edu>
2024-08-11 18:19:34 +0300

    added email.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -109,3 +109,26 @@ function openResume() {
 
 
 window.addEventListener("scroll", scrollActive())
+
+function sendMail() {
+    var params = {
+        name: document.getElementById("name").value,
+        email: document.getElementById("email").value,
+        message: document.getElementById("message").value,
+    };
+
+
+    const serviceID = "service_houjl2w";
+    const templateID = "template_tocr3uv";
+
+    emailjs
+        .send(serviceID, templateID, params)
+        .then((res) => {
+            document.getElementById("name").value = "";
+            document.getElementById("email").value = "";
+            document.getElementById("message").value = "";
+            console.log(res);
+            alert("Your message has been sent successfully!");
+        })
+        .catch((err) => console.log(err));
+}
